33bf333046f4a8b7061cb5b04c49811de553d06d
Matheus Tanaka <matheustanakanog@gmail.com>
2022-03-10 22:23:21 -0300

    fix: change folder path

---
+++ shell-scripting/userInput-comments/shell.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# read -p "Enter your name: " NAME
+
+# echo "your name is $NAME"
+
+echo "Enter your full name: "
+
+read firstName lastName 
+
+echo "Your first name is $firstName and last $lastName "
